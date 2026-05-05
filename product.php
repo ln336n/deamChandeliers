6eb0b69528663a7177b0d39a9790f4183cde4ac9
ln336n <ln336@scarletmail.rutgers.edu>
2026-05-05 16:05:14 -0400

    Update product.php

--- product.php
+++ product.php
@@ -1,24 +1,43 @@
 <?php include 'config.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Product</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
 
-<?php
-$name = $_GET['name'];
-?>
+<header>Product Details</header>
 
-<h2><?php echo $name; ?></h2>
+<nav>
+<a href="index.php">Home</a>
+<a href="products.php">Products</a>
+<a href="cart.php">Cart</a>
+<a href="about.php">About</a>
+</nav>
 
-<img src="https://via.placeholder.com/200"><br>
-<img src="https://via.placeholder.com/200"><br>
+<?php $name = $_GET['name']; ?>
+
+<h2 style="text-align:center;"><?php echo $name; ?></h2>
+
+<div style="text-align:center;">
+<img src="https://images.unsplash.com/photo-1600585154340-be6161a56a0c" width="300"><br><br>
+<img src="https://images.unsplash.com/photo-1505693416388-ac5ce068fe85" width="300"><br><br>
 
 <form method="post">
     <input type="hidden" name="product" value="<?php echo $name; ?>">
     <button type="submit" name="add">Add to Cart</button>
 </form>
+</div>
 
 <?php
 if (isset($_POST['add'])) {
     $_SESSION['cart'][] = $name;
-    echo "Added to cart!";
+    echo "<p style='text-align:center;color:gold;'>Added to cart!</p>";
 }
 ?>
 
-<a href="products.php">Back</a>
+<footer>© Dream Chandeliers</footer>
+
+</body>
+</html>
