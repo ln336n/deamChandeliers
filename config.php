2e8026a75670fe4b46cf67c947549febef0a016e
ln336n <ln336@scarletmail.rutgers.edu>
2026-05-05 15:56:13 -0400

    config.php

--- config.php
+++ config.php
@@ -1,9 +1,11 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "dream_chandeliers");
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
 session_start();
+
+// Try connecting (for XAMPP)
+$conn = @new mysqli("localhost", "root", "", "dream_chandeliers");
+
+// If connection fails (like on Render), don't crash site
+if ($conn->connect_error) {
+    $conn = null;
+}
 ?>
