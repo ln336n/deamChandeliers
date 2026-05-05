047183332dc35443ab00b9b43ff1d81e9377ccb5
ln336n <ln336@scarletmail.rutgers.edu>
2026-05-05 16:11:10 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,11 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 session_start();
 
-// Try connecting (for XAMPP)
 $conn = @new mysqli("localhost", "root", "", "dream_chandeliers");
 
-// If connection fails (like on Render), don't crash site
 if ($conn->connect_error) {
     $conn = null;
 }
